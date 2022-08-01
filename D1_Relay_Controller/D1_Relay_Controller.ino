173099afb657ebf2344500d808ea7a10c0aefcc2
Wolf Ilandl Butler <68392802+neowolfwitch@users.noreply.github.com>
2022-08-01 10:59:48 -0500

    Update D1_Relay_Controller.ino
    Added more comments to help new users, in particular with fixed IP and Multicast/Unicast.

--- D1_Relay_Controller/D1_Relay_Controller.ino
+++ D1_Relay_Controller/D1_Relay_Controller.ino
@@ -1,6 +1,10 @@
+
+/* D1/ESP-12 E1.31 Relay Control
+ * by Wolf I. Butler, Updated August, 2022
+ */
+
 #include <ESP8266WiFi.h> // a library to include
 #include <E131.h>; // a library to include https://github.com/forkineye/E131
-//Updated 08/01/2022, Wolf Butler
 
 //Set your WiFi network information below...
 const char* ssid = "WiFi_SSID"; // your network AP SSID
@@ -10,6 +14,11 @@ const char* password = "WiFi_Password"; // your network AP password
 const int universe = 1; // universe to listen to
 const int channel = 14 ; // channel to use - these are 1 lower than normal (0 = 1)
 
+/*  This script defaults to DHCP and Multicast.
+ *  Be sure to read through ALL of the comments below if you want to use a fixed IP
+ *  and/or switch to unicast. You must change the WiFi settings and E1.31 connect string!
+ */
+
 // Un-comment the following if not using DHCP, and edit accordingly.
 // Important: Those are commas, not periods!!! This is just the way the IPAddress datatype works.
 //const IPAddress ip(10,0,0,245);
@@ -17,18 +26,19 @@ const int channel = 14 ; // channel to use - these are 1 lower than normal (0 = 
 //const IPAddress gateway(10,0,0,1);
 //const IPAddress dns(8,8,8,8);
 
-//See below to set Multicast or Unicast. Default is multicast. Set to Unicast if you set IP address above!
+//See below to set Multicast or Unicast. Default is multicast!
 
 int channel_val; // the current value of the channel we are using, don't change this.
 long int num_ch_in_pkt; // number of channels in the recieved packet, don't change this.
 int output_1 = 5; // names the ESP8266 GPIO output pin we are using (5 is D1 !!!), don't change this for Relay Shield.
+
 E131 e131;
 void setup() {
   Serial.begin(115200);
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  //If you want to use a fixed IP address instead of DHCP, un-comment the following...
+  // ***** If you want to use a fixed IP address instead of DHCP, un-comment the following...
   //WiFi.config(ip, dns, gateway, netmask);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -42,22 +52,21 @@ void setup() {
   pinMode(output_1, OUTPUT); // set pin as an output
   digitalWrite(output_1, LOW); // set pin to LOW
 
-  // **** Pick one. You can either use Unicast or Multicast. If you don't know which- pick Multicast. ****
+  // ***** Pick one. You can either use Unicast or Multicast. If you don't know which- pick Multicast. ****
 //  e131.begin(E131_UNICAST, universe);   //Unicast (TCP)
   e131.begin(E131_MULTICAST, universe); //Multicast (UDP)
 }
 
-//Don't change anything below...
+// ***** Don't change anything below...
 //Any DMX value under 127 is "Off", above 127 is "On".
 void loop() {
   num_ch_in_pkt = e131.parsePacket(); // parse packet
-  if (num_ch_in_pkt) {                // if num_ch_in_pkt &amp;gt; 0
+  if (num_ch_in_pkt) {                // if num_ch_in_pkt > 0
     channel_val = (e131.data[channel]);
     Serial.println(channel_val);
     if (channel_val > 127) {
       digitalWrite(output_1, HIGH); // SSR on
       Serial.println("Power On!");
-
     }
     else {
       digitalWrite(output_1, LOW); // SSR off
